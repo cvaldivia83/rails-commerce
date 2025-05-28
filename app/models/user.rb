d938137e97e60a9fc76f6ccc0999bfc21b7b7dfd
Carla Valdivia <valdivia.carla@gmail.com>
2025-05-28 16:35:28 -0300

    [feat]
    Add username and role migration to Users Table. Add enums in role columns to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { client: 0, admin: 1, business_owner: 2, developer: 3 }
 end
