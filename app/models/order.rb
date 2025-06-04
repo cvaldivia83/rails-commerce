7025f38cbd8387449938c810461ab0eee0535b49
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-04 16:09:17 -0300

    add relation between models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
 
   enum status: { pending: 0, confirmed: 1, shipped: 2, cancelled: 3}
-  
+  belongs_to :user
+  belongs_to :customer
+  has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { business_owner: 0, admin: 1, developer: 2 }
-  
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
