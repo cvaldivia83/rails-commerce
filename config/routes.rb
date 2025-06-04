2ac7147b29901b05406935838f27a9df14da4901
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-04 16:24:14 -0300

    change root from pages#home to products#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :users
-  root to: "pages#home"
-  resources :products
+  root to: "products#index"
+  resources :products, except: [:index]
 
   resources :orders do 
     resources :order_items, except: [:destroy]
