32af187bb1bbcb5e16b27b7be7873306602f59c6
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-04 21:29:36 -0300

    add show, new, destroy actions to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,26 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_product, only: [:show, :destroy]
   def index 
     @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def destroy 
+    @product = Product.destroy
+  end
+
+  def set_product 
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_cents, :stock_quantity, :sku)
   end
 end
