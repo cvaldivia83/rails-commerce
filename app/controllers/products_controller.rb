235b8f02050204ace26a6fa75a4fa5a7c777e1e9
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-04 22:29:25 -0300

    Add cloudinary gem to project. add photo to products model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   before_action :set_product, only: [:show, :destroy]
@@ -12,6 +14,16 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create  
+    @product = Product.new(product_params)
+    @product.sku = SecureRandom.alphanumeric(10)
+    if @product.save
+      redirect_to user_path(current_user)
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy 
     @product = Product.destroy
   end
@@ -21,6 +33,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price_cents, :stock_quantity, :sku)
+    params.require(:product).permit(:name, :description, :price_cents, :stock_quantity, :photo)
   end
 end
